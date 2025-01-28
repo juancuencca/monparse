290e1debfcebeecca3a528aa9146fa8afc129809
Juan Cuenca <juan99.cuenca@gmail.com>
2025-01-27 21:26:35 -0300

    Add function parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt;
+
+pub struct Input {
+    pub text: String,
+    pub pos: usize,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Input {
+    pub fn new(text: impl Into<String>) -> Self {
+        Input { text: text.into(), pos: 0, }
     }
+}
+
+pub struct Error {
+    desc: String,
+    pos: usize,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Failed at {}: {}", self.pos, self.desc)
+    }
+}
+pub struct Parser<'a, A> {
+    pub run: Box<dyn Fn(Input) -> (Input, Result<A, Error>) + 'a>,
+}
+
+impl<'a, A> Parser<'a, A> {
+    pub fn new<F>(f: F) -> Self
+    where
+        F: Fn(Input) -> (Input, Result<A, Error>) + 'a
+    {
+        Parser { run: Box::new(f), }
+    } 
 }
