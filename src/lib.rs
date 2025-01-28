37ca25b401fa1149093565c9b33ea6cab41afbed
Juan Cuenca <juan99.cuenca@gmail.com>
2025-01-28 16:39:50 -0300

    Add aditional features to create a parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,60 @@
 use std::fmt;
 
-pub struct Input {
-    pub text: String,
-    pub pos: usize,
+struct Input {
+    text: String,
+    position: usize,
 }
 
 impl Input {
-    pub fn new(text: impl Into<String>) -> Self {
-        Input { text: text.into(), pos: 0, }
+    fn new(text: impl Into<String>) -> Self {
+        Input { text: text.into(), position: 0, }
+    }
+
+    fn sub(&self, start: usize, len: usize) -> Self {
+        let text = self.text.get(start..start + len).unwrap_or("").to_string();
+
+        Input { text, position: self.position + start, }
     }
 }
 
-pub struct Error {
-    desc: String,
-    pos: usize,
+struct Error {
+    description: String,
+    position: usize,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Failed at {}: {}", self.pos, self.desc)
+        write!(f, "Failed at {}: {}", self.position, self.description)
     }
 }
-pub struct Parser<'a, A> {
-    pub run: Box<dyn Fn(Input) -> (Input, Result<A, Error>) + 'a>,
+
+struct Parser<'a, A> {
+    run: Box<dyn Fn(Input) -> (Input, Result<A, String>) + 'a>,
 }
 
 impl<'a, A> Parser<'a, A> {
-    pub fn new<F>(f: F) -> Self
+    fn new<F>(f: F) -> Self
     where
-        F: Fn(Input) -> (Input, Result<A, Error>) + 'a
+        F: Fn(Input) -> (Input, Result<A, String>) + 'a
     {
         Parser { run: Box::new(f), }
     } 
+
+    fn fail<E>(error: E) -> Self
+    where 
+        E: Into<String> + Clone + 'a 
+    {
+        Parser::new(move |input: Input| {
+            (input, Err(error.clone().into()))
+        })
+    }
+
+    fn wrap(value: A) -> Self
+    where
+        A: Clone + 'a 
+    {
+        Parser::new(move |input: Input| {
+            (input, Ok(value.clone()))
+        })
+    }
 }
